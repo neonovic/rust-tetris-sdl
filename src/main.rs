38d69cefb2651f8d654ec2db218ea0b9c32ebcfe
Frantisek Heca <frantisek@heca.net>
2021-10-03 17:27:29 +0200

    Corrected movement of the piece with left/right/down keys - you can move freely with every game loop update, but falling of the piece down is happening once per 4 games loops. This way, I can achieve more fine grained movement, like I know from the historical tetrises.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
-use sdl2::render::{Texture, WindowCanvas};
+use sdl2::render::WindowCanvas;
 // "self" imports the "image" module itself as well as everything else we listed
-use sdl2::image::{self, InitFlag, LoadTexture};
-use std::collections::VecDeque;
+use sdl2::image::{self, InitFlag};
 use std::time::Duration;
 
 const PLAYER_MOVEMENT_SPEED: i32 = 5;
@@ -16,6 +15,7 @@ enum Direction {
     Down,
     Left,
     Right,
+    None,
 }
 
 #[derive(Debug)]
@@ -29,27 +29,29 @@ struct Piece {
     shape: Shapes,
     shape_width: i32,
     speed: i32,
-    direction: VecDeque<Direction>,
+    direction: Direction,
+    steps: u8,
 }
 
 fn render(canvas: &mut WindowCanvas, color: Color, piece: &Piece) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
-    let (width, height) = canvas.output_size()?;
+    let (width, _height) = canvas.output_size()?;
 
-    let start_position = piece.position + Point::new(width as i32 / 2, 0)
-        - Point::new(piece.shape_width * 20 / 2, 0);
+    let mut piece_position = piece
+        .position
+        .offset((width as i32 / 2) - (piece.shape_width * 20), 0);
 
     canvas.set_draw_color(Color::BLUE);
-    let mut piece_position = start_position;
     match piece.shape {
         Shapes::Elko(v) => {
             for row in v {
                 for point in row {
-                    dbg!(piece_position);
                     if point == 1 {
-                        canvas.fill_rect(Rect::new(piece_position.x, piece_position.y, 20, 20));
+                        canvas
+                            .fill_rect(Rect::new(piece_position.x, piece_position.y, 20, 20))
+                            .unwrap();
                     }
                     piece_position += Point::new(20, 0);
                 }
@@ -65,29 +67,22 @@ fn render(canvas: &mut WindowCanvas, color: Color, piece: &Piece) -> Result<(), 
 
 fn update_piece(piece: &mut Piece) {
     use self::Direction::*;
-    piece.position = match piece.direction.back() {
-        Some(Left) => piece.position.offset(-20, 0),
-        Some(Right) => piece.position.offset(20, 0),
-        Some(Up) => piece.position,
-        Some(Down) => piece.position.offset(0, 20),
+    piece.position = match piece.direction {
+        Left => piece.position.offset(-20, 0),
+        Right => piece.position.offset(20, 0),
+        Up => piece.position,
+        Down => piece.position.offset(0, 20),
         None => piece.position,
     };
-    piece.position += Point::new(0, 20);
-}
+    piece.direction = None;
 
-// Update player a fixed amount based on their speed.
-// WARNING: Calling this function too often or at a variable speed will cause the player's speed
-// to be unpredictable!
-// fn update_player(player: &mut Piece) {
-//     use self::Direction::*;
-//     player.position = match player.direction.back() {
-//         Some(Left) => player.position.offset(-player.speed, 0),
-//         Some(Right) => player.position.offset(player.speed, 0),
-//         Some(Up) => player.position.offset(0, -player.speed),
-//         Some(Down) => player.position.offset(0, player.speed),
-//         None => player.position,
-//     }
-// }
+    // Only once per 4 game loops we want to update falling of the piece one step down
+    piece.steps += 1;
+    if piece.steps == 4 {
+        piece.position += Point::new(0, 20);
+        piece.steps = 1;
+    }
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -113,7 +108,8 @@ fn main() -> Result<(), String> {
         shape: Shapes::Elko([[1, 1, 1], [1, 0, 1]]),
         shape_width: 3,
         speed: 0,
-        direction: VecDeque::new(),
+        direction: Direction::None,
+        steps: 1,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
@@ -129,9 +125,7 @@ fn main() -> Result<(), String> {
                     break 'running;
                 }
                 Event::KeyDown {
-                    keycode: Some(k),
-                    repeat: false,
-                    ..
+                    keycode: Some(k), ..
                 } => {
                     if let Some(direction) = match k {
                         Keycode::Left => Some(Direction::Left),
@@ -141,22 +135,7 @@ fn main() -> Result<(), String> {
                         _ => None,
                     } {
                         player.speed = PLAYER_MOVEMENT_SPEED;
-                        player.direction.push_back(direction);
-                    }
-                }
-                Event::KeyUp {
-                    keycode: Some(k),
-                    repeat: false,
-                    ..
-                } => {
-                    if let Some(index) = match k {
-                        Keycode::Left => find_index(&player.direction, Direction::Left),
-                        Keycode::Right => find_index(&player.direction, Direction::Right),
-                        Keycode::Up => find_index(&player.direction, Direction::Up),
-                        Keycode::Down => find_index(&player.direction, Direction::Down),
-                        _ => None,
-                    } {
-                        player.direction.remove(index);
+                        player.direction = direction;
                     }
                 }
                 _ => {}
@@ -170,17 +149,8 @@ fn main() -> Result<(), String> {
         render(&mut canvas, Color::BLACK, &player)?;
 
         // Time management!
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 2));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 8));
     }
 
     Ok(())
-}
-
-fn find_index(v: &VecDeque<Direction>, d: Direction) -> Option<usize> {
-    for (index, c) in v.iter().enumerate() {
-        if c == &d {
-            return Some(index);
-        }
-    }
-    None
 }
